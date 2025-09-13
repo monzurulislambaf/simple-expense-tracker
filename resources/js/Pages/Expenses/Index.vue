ad188a51f9b9216bb4281a710c1ab07b242c58e7
monzurulislambaf <monzurulislambaf@gmail.com>
2025-09-13 10:07:45 +0600

    fixed delete mothod

--- resources/js/Pages/Expenses/Index.vue
+++ resources/js/Pages/Expenses/Index.vue
@@ -52,7 +52,7 @@
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3';
+import { Link, router } from '@inertiajs/vue3';
 import MainLayout from '../../Layouts/MainLayout.vue';
 
 defineOptions({ layout: MainLayout });
